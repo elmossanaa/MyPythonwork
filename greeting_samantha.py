991f013b1038c9c090087b4bed495f38bb200260
Sanaa Elmosbah <80988796+elmossanaa@users.noreply.github.com>
2023-01-04 15:10:36 -0500

    Add files via upload

---
+++ greeting_samantha.py
@@ -0,0 +1,4 @@
+friend_name = 'Samantha'
+greeting = "Nice to meet you " + friend_name + " let's learn python"
+print(greeting)
+print("Fun " *3)
---
+++ testscore_problem.py
@@ -0,0 +1,5 @@
+score = int(input("enter the number of test grade you got: "))
+if(score>=90):
+    print("you get to leave the school early")
+else:
+    print("sorry, you have to stay in school")
