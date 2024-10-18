de43451ec8aedaeebabc565065eeb7286f41090e
Sanaa Elmosbah <80988796+elmossanaa@users.noreply.github.com>
2024-10-18 12:24:14 -0400

    Create while_loop_excercise.py

---
+++ while_loop_excercise.py
@@ -0,0 +1,10 @@
+#create a while loop that prints numbers 0-5, but stops the loop if the
+#number is equals 3.
+
+count = 0
+
+while count <= 5:
+    if count == 3: 
+        break
+    print(count)
+    count+=1
